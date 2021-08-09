8e7bbd72618e1f9f0b1d97345aa969f4b31c5aca
levi73159 <88604538+levi73159@users.noreply.github.com>
2021-08-08 19:32:05 -0500

    Add CollisonHandler CollisonHandler.cs

---
+++ RocketDimeson/Assets/Scripts/CollisonHandler.cs
@@ -0,0 +1,24 @@
+﻿/* copyright(c) by LeviStudios;
+ * RocketDimeson CollisonHandler copyright(c) by LeviStudios;
+ */
+using UnityEngine;
+
+public class CollisonHandler : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision other)
+	{
+		switch (other.gameObject.tag)
+		{
+			case "Friendly":
+				break;
+			
+			case "Finish":
+				Debug.Log("You Won");
+				break;
+			
+			default:
+				Debug.Log("You Crash");
+				break;
+		}
+	}
+}
--- RocketDimeson/Assets/Scripts/Movement.cs
+++ RocketDimeson/Assets/Scripts/Movement.cs
@@ -1,59 +1,57 @@
 /* copyright(c) by LeviStudios;
  * RocketDimeson Movement copyright(c) by LeviStudios;
  */
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float mainThrustSpeed = 10;
-    [SerializeField] private float rotateSpeed = 10;
-    private Rigidbody rb; //the Rigidbody attach to are player
-    private AudioSource audioSource;
+	[SerializeField] private float mainThrustSpeed = 10;
+	[SerializeField] private float rotateSpeed = 10;
+	private AudioSource audioSource;
+	private Rigidbody rb; //the Rigidbody attach to are player
 
-    // this function will setup are script
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
-    }
+	// this function will setup are script
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+	}
 
-    private void Update()
-    {
-        ProcessThrust();
-        ProcessRotation();
-    }
+	private void Update()
+	{
+		ProcessThrust();
+		ProcessRotation();
+	}
 
-    /// Move are player the relative up
-    private void ProcessThrust()
-    {
-        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            if (!audioSource.isPlaying)
-                audioSource.Play();
-            rb.AddRelativeForce(Vector3.up * (mainThrustSpeed * Time.deltaTime));
-        }
-        else
-            audioSource.Stop();
-    }
+	/// Move are player the relative up
+	private void ProcessThrust()
+	{
+		if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+		{
+			if (!audioSource.isPlaying)
+				audioSource.Play();
+			rb.AddRelativeForce(Vector3.up * (mainThrustSpeed * Time.deltaTime));
+		}
+		else
+		{
+			audioSource.Stop();
+		}
+	}
 
-    /// Rotate are player left or right when a d or left right is press
-    private void ProcessRotation()
-    {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            ApplyRotation(rotateSpeed);
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            ApplyRotation(-rotateSpeed);
-        
-    }
+	/// Rotate are player left or right when a d or left right is press
+	private void ProcessRotation()
+	{
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+			ApplyRotation(rotateSpeed);
+		else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+			ApplyRotation(-rotateSpeed);
+	}
 
-    private void ApplyRotation(float rotation)
-    {
-        rb.freezeRotation = true; // freezing rotation so we can manually rotate
-        transform.Rotate(Vector3.forward * (rotation * Time.deltaTime));
-        rb.freezeRotation = false; // unfreezing rotation so physics can take over
-    }
+	private void ApplyRotation(float rotation)
+	{
+		rb.freezeRotation = true; // freezing rotation so we can manually rotate
+		transform.Rotate(Vector3.forward * (rotation * Time.deltaTime));
+		rb.freezeRotation = false; // unfreezing rotation so physics can take over
+	}
 }
